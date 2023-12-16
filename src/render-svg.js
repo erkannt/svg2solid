86a5936ef100bda79cdd118524b75be9e3826468
Daniel Haarhoff <daniel@rknt.de>
2023-12-16 10:22:02 +0000

    Remove unused import

--- src/render-svg.js
+++ src/render-svg.js
@@ -1,5 +1,4 @@
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { SVGLoader } from 'three/examples/jsm/loaders/SVGLoader';
 
 const stokeMaterial = new THREE.LineBasicMaterial({
